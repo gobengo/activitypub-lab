f730421799056f6d78c45a147eb07545e8c54f51
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-05-04 15:34:25 -0400

    stub ucan test for invalidating a delegated publish without proof of delegation

--- src/ucan.spec.ts
+++ src/ucan.spec.ts
@@ -35,8 +35,29 @@ describe("ucan", () => {
     const validation = nameServiceUcanValidate(bobPublishAliceDid);
     assert.equal(validation, true);
   });
+  // @todo re-enable with more advanced invocation proof validation
+  xit("can invalidate delegated publish without proof", async () => {
+    const service = NewService();
+    const alice = await Issuer.generate();
+    const aliceCid1 = WHITE_LEMUR;
+    const bob = await Issuer.generate();
+    const bobPublishAliceDid = Client.invoke({
+      issuer: bob,
+      audience: service,
+      capability: {
+        can: "name/publish",
+        with: alice.did(),
+        content: aliceCid1,
+        origin: null,
+      },
+      proofs: [],
+    });
+    const validation = nameServiceUcanValidate(bobPublishAliceDid);
+    assert.equal(validation, false);
+  });
 });
 
+// @todo replace with irakli's module
 function nameServiceUcanValidate(_invocation: unknown) {
   return true;
 }
