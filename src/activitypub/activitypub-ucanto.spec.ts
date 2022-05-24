6d177a2884e16521788bb435aebfa373f8cce60b
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-05-23 22:43:45 -0700

    test submitting multiple to inbox

--- src/activitypub/activitypub-ucanto.spec.ts
+++ src/activitypub/activitypub-ucanto.spec.ts
@@ -5,23 +5,34 @@ import {
   ActivityPubUcanto,
   createAnnounceActivityPubCom,
 } from "./activitypub-ucanto.js";
+import { Agent } from "ucanto/src/api";
+
+async function exampleInboxPost(
+  activitypub: ReturnType<typeof ActivityPubUcanto>,
+  issuer: Agent
+) {
+  const response = await activitypub.inbox.post({
+    issuer,
+    audience: activitypub,
+    capability: {
+      can: "activitypub/inbox/post",
+      with: issuer.did(),
+      activity: createAnnounceActivityPubCom(),
+    },
+  });
+  assert.ok(response.ok);
+  assert.ok(response.value);
+  return response;
+}
 
 describe("activitypub-ucanto", () => {
   it("has functional inbox", async () => {
     const alice = await Issuer.generate();
     const activitypub = ActivityPubUcanto();
-    const inboxPostResponse = await activitypub.inbox.post({
-      issuer: alice,
-      audience: activitypub,
-      capability: {
-        can: "activitypub/inbox/post",
-        with: alice.did(),
-        activity: createAnnounceActivityPubCom(),
-      },
-    });
-    assert.ok(inboxPostResponse.ok);
-    assert.ok(inboxPostResponse.value);
-
+    const postIntentions = new Array(3);
+    for (const iter of postIntentions) {
+      await exampleInboxPost(activitypub, alice);
+    }
     /**
      * Now let's get the inbox
      */
@@ -34,6 +45,9 @@ describe("activitypub-ucanto", () => {
       },
     });
     assert.ok(inboxGetResponse.ok);
-    assert.equal(inboxGetResponse.value.inbox.totalItems, 1);
+    assert.equal(
+      inboxGetResponse.value.inbox.totalItems,
+      postIntentions.length
+    );
   });
 });
