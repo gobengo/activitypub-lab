3de8609b0525dfc063f7d16b7aebfa8ea81d47d6
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-05-23 22:13:49 -0700

    start activitypub

---
+++ src/activitypub/activitypub-http.spec.ts
@@ -0,0 +1,38 @@
+import { describe, it } from "mocha";
+import { universalFetch } from "../fetch.js";
+import { withHttpServer } from "../http.js";
+import * as assert from "assert";
+import * as Issuer from "../actor/issuer.js";
+import { NewService } from "../service.js";
+import * as Client from "ucanto/src/client.js";
+import * as Server from "ucanto/src/server.js";
+import * as Transport from "ucanto/src/transport.js";
+import { RequestListener } from "http";
+
+const HttpActivityPub = Object.assign(
+  (): RequestListener => {
+  return (_req, _res) => {
+    _res.writeHead(200);
+    _res.end(JSON.stringify({ message: 'activitypub'}));
+  }
+}, {
+  urls: {
+    index(baseUrl: URL) {
+      return new URL(baseUrl)
+    }
+  }
+})
+
+describe("activitypub-http", () => {
+  it("responds to / with 200 and welcome message", async () => {
+    const requestListener: RequestListener = HttpActivityPub();
+    await withHttpServer(requestListener, async (baseUrl) => {
+      const index = HttpActivityPub.urls.index(baseUrl);
+      const resp = await universalFetch(index.toString());
+      assert.equal(resp.status, 200);
+      const respBodyJson = await resp.json();
+      assert.ok(respBodyJson.message);
+      assert.ok(String(respBodyJson.message).toLowerCase().includes("activitypub"));
+    });
+  });
+});
